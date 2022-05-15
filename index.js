cee95ffbd14ecf2d7cf1aa7923ffd7d625eb343e
Davide Bulbarelli <bdavide17@gmail.com>
2022-05-15 18:17:20 +0200

    sql files generation

--- index.js
+++ index.js
@@ -2,20 +2,34 @@
 
 const csv = require('csv-parser');
 const fs = require('fs');
-const { Sequelize } = require('sequelize')
 require('dotenv').config()
-console.log(process.env.SQL_STRING)
 
-//const sequelize = new Sequelize(process.env.SQL_STRING)
+function makeid(length) {
+  var result           = '';
+  var characters       = 'ABCDEFGHIKLMNOPQRSTUVZ0123456789';
+  var charactersLength = characters.length;
+  for ( var i = 0; i < length; i++ ) {
+    result += characters.charAt(Math.floor(Math.random() * 
+charactersLength));
+ }
+ return result;
+}
+
+let counter = 0
 fs.createReadStream('iscrizioni.csv')
   .pipe(csv())
   .on('data', async (row) => {
     if(row['Iscritto per'] == 'Idrofobia') {
       const name = row['Nome']
       const surname = row['Cognome']
-      const address = row['Address']
-      console.log(name, surname, address)
-      //const [results, metadata] = await sequelize.query(`INSERT INTO players (name, surname, address) VALUES ('gino', 'pino', 'via 123')`);
+      const address = row['Indirizzo']
+      const telegram = row['Telegram']
+      const killCode = makeid(10)
+      console.log(name, surname, address, telegram, killCode)
+      
+      fs.appendFileSync('players.sql', `INSERT INTO players (id, name, surname, address) VALUES (${counter}, '${name}', '${surname}', '${address}');\n`);
+      fs.appendFileSync('idrofobia_players.sql', `INSERT INTO idrofobia_players (id, telegram_id, kill_code) VALUES (${counter}, '${telegram}', '${killCode}');\n`);
+      counter++
     }
   })
   .on('end', () => {
